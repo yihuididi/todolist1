aa9030657f3ceeb46c4e179cb6ba86fb2b825402
yihuididi <tohyihui11@yahoo.com>
2024-06-03 01:02:20 +0800

    added bootstrap, designed homepage, updated firestore

--- todolist/src/Components/Home.jsx
+++ todolist/src/Components/Home.jsx
@@ -1,24 +1,49 @@
-import React, {useState, useEffect} from 'react'
-import { auth, database } from '../firebase'
-import { getDoc, doc } from 'firebase/firestore'
+import logo from '../Images/icondarkblue.png'
+import React, {useState, useEffect} from 'react';
+import thumbnail from '../Images/thumbnail.png';
+import { auth, database } from '../firebase';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { Link } from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
+import './Home.css';
 
 export const Home = () => {
-    const [userDetails, setUserDetails] = useState()
-    const navigate = useNavigate()
+    // User state
+    const [userDetails, setUserDetails] = useState();
+
+    // User blogs
+    const [blogs, setBlogs] = useState();
+
+    const navigate = useNavigate();
     const getUserDetails = async () => {
         auth.onAuthStateChanged(async (user) => {
-            console.log(user)
-            const userDocs = await getDoc(doc(database, "Users", user.uid))
-            if (userDocs.exists()) {
-                setUserDetails(userDocs.data())
-                console.log(userDocs.data())
-            } else {
-                console.log("User not found")
+            // If signed in,...
+            if (user) {
+                const userRef = doc(database, "Users", user.uid);
+
+                // Set user details if it exist
+                const userDocs = await getDoc(userRef);
+                if (userDocs.exists()) {
+                    setUserDetails(userDocs.data());
+                    console.log(userDocs.data());
+                } else {
+                    console.log("User not found");
+                }
+
+                // Get user's blogs if they exists
+                try {
+                    const userBlogs = await getDocs(collection(userRef, "Blogs"));
+                    setBlogs(userBlogs.docs.map(blog => ({
+                        ...blog.data(),
+                        id: blog.id,
+                    })));
+                } catch (err) {
+                    console.error(err);
+                }
             }
-        })
-    }
-    useEffect(() => {getUserDetails()}, [])
+        });
+    };
+    useEffect(() => {getUserDetails()}, []);
 
     const handleLogout = async () => {
         try {
@@ -30,18 +55,40 @@ export const Home = () => {
         }
     }
 
-  return (
-    <div>
-        {userDetails ? (
-            <>
-                <h1> {userDetails.email} todolist </h1>
-                <button className="Logout-buttom" onClick={handleLogout}> Logout </button>
-            </>
-        ) : (
-            <p>Loading...</p>
-        )}
-    </div>
-  )
+    return (
+        <div>
+            {userDetails ? (
+                <>
+                    {/* topbar with logo and logout buttons. */}
+                    <div className="container-fluid top-bar">
+                        <Link to="">
+                            <img className="logo" src={logo} alt="logo"/>
+                        </Link>
+                        <button className="Logout-button float-end" onClick={handleLogout}>Logout</button>
+                    </div>
+
+                    {/* div for greeting logged in user. */}
+                    <div className="container mx-auto text-center greeting">
+                        <h1>Good day, {userDetails.email}</h1>
+                    </div>
+
+                    {/* div for listing out all user's created blogs. */}
+                    <div className="container mx-auto text-center">
+                        <div className="row row-cols-4">
+                            {blogs?.map(blog => (
+                                <div className="col">
+                                    <img class="thumbnail" src={thumbnail} alt="thumbnail"/>
+                                    <h6>{blog.name}</h6>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    )
 }
 
 export default Home
